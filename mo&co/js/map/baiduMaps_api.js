e2b0d28bb62c9684cae95746e4f130d51894fbf9
zhuoya165 <1903257579@qq.com>
2018-05-17 21:05:43 +0800

    b

--- mo&co/js/map/baiduMaps_api.js
+++ mo&co/js/map/baiduMaps_api.js
@@ -1,70 +1,35 @@
-/*百度地图制作声明：
-	1,只需要更换中心点坐标和标注点数组坐标；
-	  拾取坐标系统:http://api.map.baidu.com/lbsapi/getpoint/index.html
-	  
-	2,修改地图提示框内容；
------------------------------------------------------------
-*/
 
+// 百度地图API功能
+var map = new BMap.Map('allmap');
+var poi = new BMap.Point(113.333289,23.153658);
+map.centerAndZoom(poi, 15);
+map.enableScrollWheelZoom();
 
-//创建和初始化地图函数：
-    function initMap(){
-      createMap();//创建地图
-      setMapEvent();//设置地图事件
-      addMapControl();//向地图添加控件
-      addMapOverlay();//向地图添加覆盖物
-    }
-    function createMap(){ 
-      map = new BMap.Map("map"); 
-      map.centerAndZoom(new BMap.Point(113.333289,23.153658),15);
-    }
-    function setMapEvent(){
-      map.enableScrollWheelZoom();
-      map.enableKeyboard();
-      map.enableDragging();
-      map.enableDoubleClickZoom()
-    }
-    function addClickHandler(target,window){
-      target.addEventListener("click",function(){
-        target.openInfoWindow(window);
-      });
-    }
-    function addMapOverlay(){
-      var markers = [
-        {content:"地址：广州市天河区林和中路63号东方宝泰广场B1层<br/> 电话：20-38096169<br/> 营业时间：周一至周日 10:00-22:00<br/> Email：www.mo-co.com<br/>",title:"Moco摩安珂(东方宝泰广场店)",imageOffset: {width:0,height:3},position:{lat:23.153658,lng:113.329696}}
-      ];
-      for(var index = 0; index < markers.length; index++ ){
-        var point = new BMap.Point(markers[index].position.lng,markers[index].position.lat);
-        var marker = new BMap.Marker(point,{icon:new BMap.Icon("http://api.map.baidu.com/lbsapi/createmap/images/icon.png",new BMap.Size(20,25),{
-          imageOffset: new BMap.Size(markers[index].imageOffset.width,markers[index].imageOffset.height)
-        })});
-        var label = new BMap.Label(markers[index].title,{offset: new BMap.Size(25,5)});
-        var opts = {
-          width: 200,
-          title: markers[index].title,
-          enableMessage: false
-        };
-        var infoWindow = new BMap.InfoWindow(markers[index].content,opts);
-        marker.setLabel(label);
-        addClickHandler(marker,infoWindow);
-        map.addOverlay(marker);
-      };
-    }
-    //向地图添加控件
-    function addMapControl(){
-      var scaleControl = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
-      scaleControl.setUnit(BMAP_UNIT_IMPERIAL);
-      map.addControl(scaleControl);
-      var navControl = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
-      map.addControl(navControl);
-      var overviewControl = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:true});
-      map.addControl(overviewControl);
-    }
-    var map;
-      initMap();
+var content = '<div style="margin:0;line-height:20px;padding:2px;">' +
+                '<img src="/images/maps_logo.png" alt="" style="float:right;zoom:1;overflow:hidden;width:100px;height:100px;margin-left:3px;"/>' +
+                '地址：广州市天河区林和中路63号东方宝泰广场B1层<br/> 电话：20-38096169<br/> 营业时间：周一至周日 10:00-22:00<br/> Email：www.mo-co.com' +
+              '</div>';
 
-
-
+//创建检索信息窗口对象
+var searchInfoWindow = null;
+searchInfoWindow = new BMapLib.SearchInfoWindow(map, content, {
+    title: "Moco摩安珂(东方宝泰广场店)",      //标题
+    width: 290,             //宽度
+    height: 105,              //高度
+    panel: "panel",         //检索结果面板
+    enableAutoPan: true,     //自动平移
+    searchTypes: [
+        BMAPLIB_TAB_SEARCH,   //周边检索
+        BMAPLIB_TAB_TO_HERE,  //到这里去
+        BMAPLIB_TAB_FROM_HERE //从这里出发
+    ]
+});
+var marker = new BMap.Marker(poi); //创建marker对象
+marker.enableDragging(); //marker可拖拽
+marker.addEventListener("click", function (e) {
+    searchInfoWindow.open(marker);
+})
+map.addOverlay(marker); //在地图中添加marker
 
 
 
